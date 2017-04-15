727f27d65fef05dd37d1bd32c9a1158c2470ab44
Richard Hardman <chevellebro1@gmail.com>
2017-04-15 11:42:50 -0400

    Update Z Steps per Unit

--- Configuration.h
+++ Configuration.h
@@ -749,7 +749,7 @@
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {48.14169,48.14169,1512.42,97.1402}  // default steps per unit for Ultimaker
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {48.14169,48.14169,2560,97.1402}  // default steps per unit for Ultimaker
 #define DEFAULT_MAX_FEEDRATE          {100, 100, 10, 10}    // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {3000,3000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
